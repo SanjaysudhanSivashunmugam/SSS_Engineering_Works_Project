f738f50747a91e203390c0ada7dfe4861dd1a8e5
SanjaysudhanSivashunmugam <sanjaysudhan.sivashunmugam@gmail.com>
2024-12-31 15:31:41 +0530

    fixed bug

--- src/Component/Card.jsx
+++ src/Component/Card.jsx
@@ -1,9 +1,9 @@
-
+import vision from "/src/assets/vision.png"
 
 function Card () {
     return(
         <div className="lg:w-[28%] border-[0.1px] rounded-lg border-gray-400 bg-white flex flex-col justify-center items-center px-7 py-10  shadow-2xl">
-              <div><img className="w-20 h-20" src="./public/assets/vision.png" alt="" /></div>
+              <div><img className="w-20 h-20" src={vision} alt="" /></div>
               <p className="pt-8 text-justify ">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi itaque eligendi molestiae culpa distinctio unde atque quidem mollitia eveniet odio!</p>
         </div>
     );
--- src/Component/Product.jsx
+++ src/Component/Product.jsx
@@ -1,3 +1,11 @@
+import p1 from '../assets/p1.png'
+import p2 from '../assets/p2.jpg'
+import p3 from '../assets/p3.png'
+import p4 from '../assets/p4.png'
+import p5 from '../assets/p5.png'
+import p6 from '../assets/p6.png'
+import p7 from '../assets/p7.png'
+import p8 from '../assets/p8.png'
 
 function Products () {
     return (
@@ -6,37 +14,37 @@ function Products () {
             <div>
                 <div className="lg:flex justify-between pt-10 space-y-14 lg:space-y-0">
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="./public/assets/p1.png" alt="" />
+                    <img className="rounded-xl" src={p1} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Bore Cap</h1>
                 </div>
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../public/assets/p2.jpg" alt="" />
+                    <img className="rounded-xl" src={p2} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Bore Clamp</h1>
                 </div>
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../src/assets/p3.png" alt="" />
+                    <img className="rounded-xl" src={p3} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Clamp</h1>
                 </div>
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../src/assets/p4.png" alt="" />
+                    <img className="rounded-xl" src={p4}  alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Gate</h1>
                 </div>
             </div>
             <div className="lg:flex justify-between pt-14 space-y-14 lg:space-y-0">
             <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../src/assets/p5.png" alt="" />
+                    <img className="rounded-xl" src={p5} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Step Clamp</h1>
                 </div>
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../src/assets/p6.png" alt="" />
+                    <img className="rounded-xl" src={p6} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Apartment Clamp</h1>
                 </div>
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../src/assets/p7.png" alt="" />
+                    <img className="rounded-xl" src={p7} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">Pipe Clamp</h1>
                 </div>
                 <div className="lg:w-[22%] shadow-2xl p-3 rounded-lg border-2">
-                    <img className="rounded-xl" src="../src/assets/p8.png" alt="" />
+                    <img className="rounded-xl" src={p8} alt="" />
                     <h1 className="text-center font-semibold text-xl py-3">All Grill Works</h1>
                 </div>
             </div>
